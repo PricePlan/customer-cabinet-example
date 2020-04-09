b7649c25d4718d0528efd6d5b2b5fb446d93ec70
Stanislav Posonsky <psk@cpositive.ru>
2020-04-09 19:45:01 +0800

    Add comments

--- simple.py
+++ simple.py
@@ -14,12 +14,16 @@ try:
 except ImportError:
     import config
 
+# Включение логгирования
 logging.basicConfig(level=logging.DEBUG)
 LOG = logging.getLogger('simple')
+
+# HTTP путь ресурса, возвращающего редирект
 PATH_REDIRECT_TO = f'{config.PERSONAL_AREA_PREFIX}/redirect-to-pp'
+
 routes = web.RouteTableDef()
 
-
+# Это основная страница кабинета
 @routes.get(config.PERSONAL_AREA_PREFIX)
 @aiohttp_jinja2.template('main.jinja2')
 async def main(request):
@@ -31,15 +35,18 @@ async def main(request):
         'prefix': config.PERSONAL_AREA_PREFIX
     }
 
-
+# Ресурс, отвечающий за аутентификацию пользователя по ссылке и
+# перенаправление на страницу кабинета в PricePlan
 @routes.get(PATH_REDIRECT_TO)
 async def redirect(request):
     """Return redirect to PricePlan token-authentication resource."""
+    # Создаём сессию для служебного пользователя
     async with aiohttp.ClientSession() as session:
+        # Отправляем запрос для аутентификации служебного пользователя
         async with session.post(url=f'{config.PP_URL}/api/login',
                                 json={'user': config.SERVICE_USERNAME,
                                       'password': config.SERVICE_PASSWORD}):
-
+            # Следующим запросом получаем токен для целевого пользователя
             async with session.get(
                     f'{config.PP_URL}/api/users/{config.USER_ID}/reset_token/'
                     ) as resp:
@@ -47,13 +54,14 @@ async def redirect(request):
                 # LOG.info(f'Response JSON: {user_data}')
 
                 # Возвращаем броузеру редирект на страницу авторизации
-                # пользователя по токену:
+                # целевого пользователя по токену:
                 token = user_data['data']['token']
                 raise web.HTTPFound(
                     location=f'{config.PP_URL}/auth-key/{token}/')
 
-
+# Ресурс, необходимый для отдачи iframeResizer.min.js
 routes.static(f'{config.PERSONAL_AREA_PREFIX}/js', config.BASE_DIR)
+
 app = web.Application()
 aiohttp_jinja2.setup(
     app,
